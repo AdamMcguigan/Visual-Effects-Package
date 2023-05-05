638e33fdd22a45a5e5ddd3120d4452dafccca135
Adam Mcguigan <c00248515@itcarlow.ie>
2023-05-05 17:01:46 +0100

    Minor Changes

--- Assets/Tests/TestScript.cs
+++ Assets/Tests/TestScript.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
+using UnityEngine.VFX;
 
 public class TestScript
 {
@@ -37,6 +38,90 @@ public class TestScript
         yield return new WaitForSeconds(0.3f);
 
         Assert.IsTrue(isPlaying);
+    }
+
+    [UnityTest]
+    public IEnumerator TestArcane()
+    {
+        GameObject arcane = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Arcane Portal"));
+        arcane.GetComponent<VisualEffect>().Play();
+
+       // bool isPlaying = arcane.enabled;
+        yield return new WaitForSeconds(3.0f);
+
+        UnityEngine.Assertions.Assert.IsTrue(arcane.GetComponent<VisualEffect>().isActiveAndEnabled);
+    }
+
+    [UnityTest]
+    public IEnumerator TestLightning()
+    {
+        GameObject Lightning = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Electricity"));
+        Lightning.GetComponent<VisualEffect>().Play();
+
+        // bool isPlaying = arcane.enabled;
+        yield return new WaitForSeconds(1.3f);
+
+        UnityEngine.Assertions.Assert.IsTrue(Lightning.GetComponent<VisualEffect>().isActiveAndEnabled);
+    }
+
+    [UnityTest]
+    public IEnumerator TestFog()
+    {
+        GameObject fog = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/FogVFX"));
+        fog.GetComponent<VisualEffect>().Play();
+
+        // bool isPlaying = arcane.enabled;
+        yield return new WaitForSeconds(2.0f);
+
+        UnityEngine.Assertions.Assert.IsTrue(fog.GetComponent<VisualEffect>().isActiveAndEnabled);
+    }
+
+    [UnityTest]
+    public IEnumerator TestCharacter()
+    {
+        GameObject character = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Character Effect"));
+        character.GetComponent<VisualEffect>().Play();
+
+        yield return new WaitForSeconds(0.5f);
+
+        UnityEngine.Assertions.Assert.IsTrue(character.GetComponent<VisualEffect>().isActiveAndEnabled);
+    }
+
+    [UnityTest]
+    public IEnumerator TestPortal()
+    {
+        GameObject portal = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Portal"));
+        portal.GetComponent<VisualEffect>().Play();
+
+        // bool isPlaying = arcane.enabled;
+        yield return new WaitForSeconds(4.0f);
+
+        UnityEngine.Assertions.Assert.IsTrue(portal.GetComponent<VisualEffect>().isActiveAndEnabled);
+    }
+
+    [UnityTest]
+    public IEnumerator TestDust()
+    {
+        GameObject dust = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Dust Particle System"));
+        dust.GetComponent<ParticleSystem>().Play();
+
+        // bool isPlaying = arcane.enabled;
+        yield return new WaitForSeconds(0.2f);
+
+        UnityEngine.Assertions.Assert.IsTrue(dust.GetComponent<ParticleSystem>().isPlaying);
+    }
+
+    [UnityTest]
+    public IEnumerator TestDustStop()
+    {
+        GameObject dust = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Dust Particle System"));
+        dust.GetComponent<ParticleSystem>().Play();
+
+        // bool isPlaying = arcane.enabled;
+        yield return new WaitForSeconds(1.0f);
+
+
+        UnityEngine.Assertions.Assert.IsFalse(dust.GetComponent<ParticleSystem>().isPlaying);
     }
 
     // A Test behaves as an ordinary method
